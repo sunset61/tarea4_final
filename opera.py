83d4ab47be63b8dec5a42ef5bc2a04876fa0caf9
Alonso R <alonso.vladimir.94@gmail.com>
2020-09-06 00:34:13 -0400

    tarea4.1.3

--- opera.py
+++ opera.py
@@ -2,3 +2,8 @@ x=5
 y=2
 print(f'Sean x=5 e y=2')
 print(f'La suma de x e y es x+y={x+y}')
+print(f'El producto de x e y es x*y={x*y}')
+print(f'La división exacta de x en y es x/y={x/y}')
+print(f'x elevado a y es x**y={x**y}')
+print(f'El módulo de x en y es x%y={x%y}')
+
