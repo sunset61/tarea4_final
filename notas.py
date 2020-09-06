a42e73fcdef49a13f0592dd86b720ef29a0e5b7c
Alonso R <alonso.vladimir.94@gmail.com>
2020-09-06 19:14:18 -0400

    tarea4.2.1

---
+++ notas.py
@@ -0,0 +1,7 @@
+notas={1:('Nombre:','  Aquiles','  Elba','  Alan','  Armando'),2:('Apellido:','  Baeza','  Zurita','  Brito','  Casas')}
+v=(1,2)
+for i in v:
+	print((notas[i])[0])
+	print((notas[i])[2])
+
+
