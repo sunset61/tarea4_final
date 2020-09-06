45bf6305aa761069a618b9a44167a4aebc9e8021
Alonso R <alonso.vladimir.94@gmail.com>
2020-09-06 19:21:11 -0400

    tarea4.2.3

--- notas.py
+++ notas.py
@@ -1,5 +1,5 @@
-notas={1:('Nombre:','  Aquiles','  Elba','  Alan','  Armando'),2:('Apellido:','  Baeza','  Zurita','  Brito','  Casas')}
-v=(1,2)
+notas={1:('Nombre:','  Aquiles','  Elba','  Alan','  Armando'),2:('Apellido:','  Baeza','  Zurita','  Brito','  Casas'),3:('Programación:','  4.5','  5.8','  7.0','  6.1'),4:('Física:','  5.9','  4.6','  7.0','  5.0')}
+v=(1,2,3,4)
 for i in v:
 	print((notas[i])[0])
 	print((notas[i])[2])
